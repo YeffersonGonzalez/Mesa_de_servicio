7e04f7c269c652fdf9891ff0655d78ab59a30747
YeffersonGonzalez <yeferson172003@gmail.com>
2023-11-17 20:59:34 -0500

    Changes to be committed: 	modified:   api/libros_listado_api.php 	new file:   api/usuarioUpdateNaye.php 	new file:   clientesUpdate.php

--- api/libros_listado_api.php
+++ api/libros_listado_api.php
@@ -1,11 +1,17 @@
 <?php
     include "../app/libros-listados-service.php";
+    include "../config/config.php";
+    
     $objAPI = new librosAPI();
 
-    
+    $method = $_SERVER['REQUEST_METHOD'];
     header("Content-Type: Application/json");
-    
-            $objAPI->getAllLibros();                        
+    if ($method == 'GET') {
+            $objAPI->getAllLibros();                          
+    }else{
+        echo json_encode(array("data"=>null, "error"=>"3", "msg"=>$errorResponse[3] ));
+    } 
             
+   ?>         
 
         
